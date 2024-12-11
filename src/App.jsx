b41e6e62e90f51709e7ebb06309560d998905dbc
abadanaf <abadanafchala@gmail.com>
2024-12-11 19:14:07 +0300

    the footer page added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from './pages/Home';
 import Navbar from './component/Navbar';
+import Footer from './component/Footer';
 
 const App =() => {
   return (
@@ -11,6 +12,7 @@ const App =() => {
       <Routes>
         <Route path ="/" element={<Home/>}></Route>
       </Routes>
+      <Footer/>
     </BrowserRouter>
   );
 }
--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
+import { FaFacebookF, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <div>
-      
+      <div>
+        <div>
+            <h2>Oromia Tourism Agency</h2>
+            <p>Biiroo Aadaaf Tuurizimii Oromiyaa OTA BATO</p>
+        </div>
+        <div>
+            <h2>Quick Links</h2>
+            <div>
+                <Link to="/">Home</Link>
+                <Link to="/gallery">Gallery</Link>
+                <Link to="/contact">Contact</Link>
+                <Link to="/about">About</Link>
+            </div>
+        </div>
+        <div>
+            <h3>Follow Us</h3>
+            <div>
+                <FaFacebookF/>
+                <FaInstagram/>
+                <FaTwitter/>
+                <FaYoutube/>
+            </div>
+            <form>
+                <input type="email" placeholder='Enter Your Email' />
+                <button>Subscribe</button>
+            </form>
+        </div>
+      </div>
+      <div>
+        <p>&copy; Abadan Chala. All rights reserved.</p>
+        <div>
+            <Link to="">Privacy Policy</Link>
+            <Link to="">Terms Of Services</Link>
+        </div>
+      </div>
     </div>
   );
 }
